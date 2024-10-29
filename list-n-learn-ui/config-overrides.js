3fb6a16823cfbe0ba124ba242ae9bcd6be0f1f4e
xaridar <xaridara@gmail.com>
2024-10-29 02:42:11 -0400

    sign-up sequence (plus some nasty webpack stuff)

---
+++ list-n-learn-ui/config-overrides.js
@@ -0,0 +1,17 @@
+const webpack = require('webpack');
+module.exports = function override(config, env) {
+	//do stuff with the webpack config...
+
+	config.resolve.fallback = {
+		crypto: require.resolve('crypto-browserify'),
+		stream: require.resolve('stream-browserify'),
+	};
+	config.plugins.push(
+		new webpack.ProvidePlugin({
+			process: 'process/browser',
+			Buffer: ['buffer', 'Buffer'],
+		}),
+	);
+
+	return config;
+};
--- list-n-learn-ui/src/routes/Home.jsx
+++ list-n-learn-ui/src/routes/Home.jsx
@@ -1,25 +1,74 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import ReactLoading from 'react-loading';
+import { generateUsername } from 'unique-username-generator';
 import { checkUser } from '../util';
 
 export const Home = () => {
-    useEffect(() => {
-        const fetchUser = async () => {
-            const user = localStorage.getItem('lnl-user');
-            if (user) {
-                if (await checkUser(user)) {
-                    setUsername(user);
-                    const sets = await fetch(`/sets?user=${user}`);
-                    setSets(await sets.json());
-                }
-                else localStorage.removeItem('lnl-user');
-            }
-        }
-        fetchUser();
-    }, []);
+	useEffect(() => {
+		const fetchUser = async () => {
+			const user = localStorage.getItem('lnl-user');
+			if (user) {
+				if (await checkUser(user)) {
+					setUsername(user);
+					const sets = await fetch(`/sets?user=${user}`);
+					setSets(await sets.json());
+				} else localStorage.removeItem('lnl-user');
+			}
+			setLoading(false);
+		};
+		fetchUser();
+	}, []);
 
-    const [username, setUsername] = useState(null);
-    const [sets, setSets] = useState([]);
-    return (
-        username ? <p>{sets.map(s => s.title)}</p> : <p>nah</p>
-    );
-}
+	const createUser = async () => {
+		setLoading(true);
+		let uniqueUsername = false;
+		let username;
+		while (!uniqueUsername) {
+			// Extremely low chance of collisions (random selection of approx. 5.39B options)
+			username = generateUsername('', 2);
+			const res = await fetch('/user', {
+				method: 'POST',
+				body: JSON.stringify({ username }),
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+				},
+			});
+			const json = await res.json();
+			console.log(json);
+			if (json.success) uniqueUsername = true;
+		}
+		setUsername(username);
+		localStorage.setItem('lnl-user', username);
+		setLoading(false);
+	};
+	const signIn = () => {};
+
+	const [username, setUsername] = useState(null);
+	const [loading, setLoading] = useState(true);
+	const [sets, setSets] = useState([]);
+	return loading ? (
+		<ReactLoading
+			type='spinningBubbles'
+			width='30vw'
+			className='loading'
+		/>
+	) : (
+		<div className='App home-page'>
+			{username ? (
+				<>
+					<p>{username}</p>
+					<p>{sets.map((s) => s.title)}</p>
+				</>
+			) : (
+				<>
+					<h1>List n' Learn</h1>
+					<div className='buttons-row'>
+						<button onClick={createUser}>Create Account</button>
+						<button onClick={signIn}>Log into Existing Account</button>
+					</div>
+				</>
+			)}
+		</div>
+	);
+};
