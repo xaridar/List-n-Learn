17fb67f9b278e58efbe712721ad919986369c00b
xaridar <xaridara@gmail.com>
2024-10-29 00:12:09 -0400

    proxy fixed

--- list-n-learn-ui/src/routes/ViewSet.jsx
+++ list-n-learn-ui/src/routes/ViewSet.jsx
@@ -1,28 +1,33 @@
-import React, {useState, useEffect} from 'react';
-import {Flashcard} from '../components/Flashcard';
-import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Flashcard } from '../components/Flashcard';
+import { useParams } from 'react-router-dom';
 
 export const ViewSet = () => {
-    const getAll = () => {
-        fetch('/cards')
-            .then(res => res.json())
-            .then(res => {
-                setCards(cards => res);
-            });
-    }
-    
-    const {id: setID} = useParams();
-    // if current username exists setOwner, then do all this
-    
-    const [cards, setCards] = useState([]);
-    useEffect(() => {
-        getAll();
-    }, []);
-    return (
-        <div className='App'>
-            <div className="view">
-                {cards.map(c => <Flashcard term={c.term} definition={c.definition} key={c._id}></Flashcard>)}
-            </div>
-        </div>
-    );
-}
+	const getAll = () => {
+		fetch('/cards')
+			.then(res => res.json())
+			.then(res => {
+				setCards(cards => res);
+			});
+	};
+
+	const { id: setID } = useParams();
+	// if current username exists setOwner, then do all this
+
+	const [cards, setCards] = useState([]);
+	useEffect(() => {
+		getAll();
+	}, []);
+	return (
+		<div className='App'>
+			<div className='view'>
+				{cards.map(c => (
+					<Flashcard
+						term={c.term}
+						definition={c.definition}
+						key={c._id}></Flashcard>
+				))}
+			</div>
+		</div>
+	);
+};
