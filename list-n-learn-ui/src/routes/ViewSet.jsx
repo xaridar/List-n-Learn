4bd6470ac98525f4a47e5e9f2e40bdb7c583852b
xaridar <xaridara@gmail.com>
2024-11-04 23:11:42 -0500

    bugfixes

--- list-n-learn-ui/src/routes/ViewSet.jsx
+++ list-n-learn-ui/src/routes/ViewSet.jsx
@@ -1,50 +1,49 @@
-import React, {useState, useEffect} from 'react';
-import { useSearchParams } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { FullFlashcard } from '../components/FullFlashcard';
 
 export const ViewSet = () => {
-    useEffect(() => {
-        getSet();
-    });
+	const [searchParams] = useSearchParams();
+	const setID = searchParams.get('id');
+	useEffect(() => {
+		const getSet = () => {
+			console.log(setID);
+			fetch(`/set?id=${setID}`)
+				.then((res) => res.json())
+				.then((res) => {
+					setInfo((set) => res);
+					setCards((set) => res.cards);
+				});
+		};
+		getSet();
+	}, [setID]);
 
-    // retrieves all cards in db
-    // const getAll = () => {
-    //     fetch('/allcards')
-    //         .then(res => res.json())
-    //         .then(res => {
-    //             setCards(cards => res);
-    //         })
-    // }
+	// retrieves all cards in db
+	// const getAll = () => {
+	//     fetch('/allcards')
+	//         .then(res => res.json())
+	//         .then(res => {
+	//             setCards(cards => res);
+	//         })
+	// }
 
-    const getSet = () => {
-        console.log(setID);
-        fetch(`/set?id=${setID}`)
-            .then(res => res.json())
-            .then(res => {
-                setInfo(set => res);
-                setCards(set => res.cards);
-                
-            }) 
-    }
+	// if current username exists setOwner, then do all this
 
-    const [searchParams] = useSearchParams();
-    const setID = searchParams.get('id');
-    // if current username exists setOwner, then do all this
-    
-    const [cards, setCards] = useState([]);
-    const [set, setInfo] = useState([]);
+	const [cards, setCards] = useState([]);
+	const [set, setInfo] = useState([]);
 
-    return (
-        <div>
-            <div className="setInformation">
-                <h1 className='title'>{set.title}</h1>
-                <div className='setDescription'>{set.description}</div>
-            </div>
-            {cards.map(c => (<FullFlashcard 
-                            term={c.term} 
-                            definition={c.definition} 
-                            key={c._id}></FullFlashcard>
-            ))}
-        </div>
-    );
-}
+	return (
+		<div>
+			<div className='setInformation'>
+				<h1 className='title'>{set.title}</h1>
+				<div className='setDescription'>{set.description}</div>
+			</div>
+			{cards.map((c) => (
+				<FullFlashcard
+					term={c.term}
+					definition={c.definition}
+					key={c._id}></FullFlashcard>
+			))}
+		</div>
+	);
+};
--- schema.js
+++ schema.js
@@ -1,26 +1,21 @@
 const mongoose = require('mongoose');
 
 const defineSchemas = () => {
-    const cardSchema = new mongoose.Schema({
-        term: String,
-        definition: String,
-        favorite: Boolean,
-    });
-    Card = mongoose.model('Card', cardSchema);
-    const userSchema = new mongoose.Schema({
-        username: {type: String, unique: true},
-    });
-    const setSchema = new mongoose.Schema({
-        title: String,
-        description: String,
-        user: String,
-        cards: [{type: mongoose.Schema.Types.ObjectId, ref: 'Card'}]
-    });
-    return [
-        Card,
-        mongoose.model('User', userSchema),
-        mongoose.model('Set', setSchema),
-    ];
-}
+	const cardSchema = new mongoose.Schema({
+		term: String,
+		definition: String,
+		favorite: Boolean,
+	});
+	const userSchema = new mongoose.Schema({
+		username: { type: String, unique: true },
+	});
+	const setSchema = new mongoose.Schema({
+		title: String,
+		description: String,
+		user: String,
+		cards: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Card' }],
+	});
+	return [mongoose.model('Card', cardSchema), mongoose.model('User', userSchema), mongoose.model('Set', setSchema)];
+};
 
-module.exports = {defineSchemas};
+module.exports = { defineSchemas };
