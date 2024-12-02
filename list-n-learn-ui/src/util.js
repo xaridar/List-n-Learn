43d53c939e3b01ef8f0a67d73ca2850083b21174
Elliot Topper <xaridara@gmail.com>
2024-12-02 15:39:14 -0500

    util.js commenting

--- list-n-learn-ui/src/util.js
+++ list-n-learn-ui/src/util.js
@@ -1,6 +1,15 @@
+/**
+ * This file defines a number of utility functions for use across frontend components.
+ */
+
 import { useEffect, useState } from 'react';
 import useLocalStorage from 'react-use-localstorage';
 
+/**
+ * Checks if a user exiss in the database
+ * @param {string} username the username to check in the database
+ * @returns {boolean} true if the user requested exists
+ */
 export const checkUser = async (username) => {
 	const res = await fetch(`/user?name=${username}`);
 	const json = await res.json();
@@ -9,11 +18,22 @@ export const checkUser = async (username) => {
 
 let speechRate = 1;
 
+/**
+ * Sets speech playback rate
+ * @param {number} rate the rate (0 - 10) to set playback to
+ */
 export const setSpeed = (rate) => {
 	speechRate = rate;
 	speakPhrase(`Audio playback speed updated to ${rate}.`);
 };
 
+/**
+ * Uses TTS to speak a phrase, and optionally captures the user's response
+ * @param {string} phrase a phrase to be spoken by TTS
+ * @param {boolean} response whether this call to speakPhrase should return the user's response
+ * @param {SpeechRecognition} recog the SpeechRecognition instance to be used for capturing user responses
+ * @returns {string | void} void if no user response requested; otherwise, returns the spoken response
+ */
 export const speakPhrase = async (phrase, response = false, recog = null) => {
 	window.speechSynthesis.cancel();
 	const utterance = new SpeechSynthesisUtterance(phrase);
@@ -34,6 +54,12 @@ export const speakPhrase = async (phrase, response = false, recog = null) => {
 		});
 };
 
+/**
+ * Searches an array of cards for the first card with a specified term
+ * @param {[Card]} cards an array of cards to search through
+ * @param {string} term the term to search for
+ * @returns {Card | -1} the first card found with the given term, or -1 if not found
+ */
 export const getCard = (cards, term) => {
 	for (const card of cards) {
 		if (card.term.trim().toLowerCase() === term.trim().toLowerCase()) {
@@ -43,9 +69,10 @@ export const getCard = (cards, term) => {
 	return -1;
 };
 
-let logoutFn = () => null;
-let loginFn = () => null;
-let helpFn = () => null;
+/**
+ * Defines a React hook for the animation state of the application
+ * @returns {[boolean, Dispatch<String>]} returns the defined hook
+ */
 export const useAnim = () => {
 	const [anim, setAnim] = useLocalStorage('lnl-anim', 'true');
 	const [boolAnim, setBoolAnim] = useState(anim !== 'false');
@@ -54,6 +81,11 @@ export const useAnim = () => {
 	}, [anim]);
 	return [boolAnim, setAnim];
 };
+
+// local functions are managed to allow for default command definition
+let logoutFn = () => null;
+let loginFn = () => null;
+let helpFn = () => null;
 
 export const registerLogout = (logout) => {
 	logoutFn = logout;
